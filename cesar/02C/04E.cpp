789c85792b4825983cd0c62801c0c17f3d2d5b8a
mxs2 <mat.xaviersantos@gmail.com>
2025-09-02 21:44:26 -0300

    refact: 04E and 05E

--- cesar/02C/04E.cpp
+++ cesar/02C/04E.cpp
@@ -3,49 +3,49 @@
 using namespace std;
 
 struct No {
-  int valor;
-  struct No* prox;
+    int valor;
+    struct No* prox;
 };
 
 No* inserirInicio(No* lista, int valor) {
-  No* novo = new No;
-  novo->valor = valor;
-  novo->prox = lista;
+    No* novo = new No;
+    novo->valor = valor;
+    novo->prox = lista;
 
-  return novo;
+    return novo;
 }
 
 No* inserirFim(No* lista, int valor) {
-  No* temp = lista;
-  while (temp->prox != NULL) {
-    temp = temp->prox;
-  }
+    No* temp = lista;
+    while (temp->prox != NULL) {
+        temp = temp->prox;
+    }
 
-  No* novo = new No;
-  novo->valor = valor;
-  novo->prox = NULL;
-  temp->prox = novo;
+    No* novo = new No;
+    novo->valor = valor;
+    novo->prox = NULL;
+    temp->prox = novo;
 
-  return lista;
+    return lista;
 }
 
 void exibirLista(No* lista) {
-  No* temp = lista;
-  while (temp != NULL) {
-    cout << temp->valor << " ";
-    temp = temp->prox;
-  }
-  cout << "NULL" << endl;
+    No* temp = lista;
+    while (temp != NULL) {
+        cout << temp->valor << " ";
+        temp = temp->prox;
+    }
+    cout << "NULL" << endl;
 }
 
 int main() {
-  No* lista = NULL;
-  lista = inserirInicio(lista, 10);
-  lista = inserirInicio(lista, 20);
-  lista = inserirInicio(lista, 30);
-  lista = inserirFim(lista, 40);
-  lista = inserirFim(lista, 50);
-  exibirLista(lista);
+    No* lista = NULL;
+    lista = inserirInicio(lista, 10);
+    lista = inserirInicio(lista, 20);
+    lista = inserirInicio(lista, 30);
+    lista = inserirFim(lista, 40);
+    lista = inserirFim(lista, 50);
+    exibirLista(lista);
 
-  return 0;
+    return 0;
 }
